18968761287880353ee8d7476944ad2e2c06c3c4
rahaf-q11 <117819822+rahaf-q11@users.noreply.github.com>
2022-11-09 14:13:48 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function mooncakeSays(message){
+ let mooncake='(o.o)';
+ if( !message) {
+   return '{mooncake} cookity?';
+ }
+  return '${mooncake} ${message}?';
+}
+module.exports.mooncakSays = mooncakeSays;
+
+  
